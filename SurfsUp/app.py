d7cbe430195d6fd55bbf6cab69ecd9fcd86ff918
Grant Gorham <grantegorham26@gmail.com>
2024-04-09 00:31:46 -0600

    created welcome page and precipitation api

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -13,7 +13,7 @@ from flask import Flask, jsonify
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -26,7 +26,7 @@ Station = Base.classes.station
 
 
 # Create our session (link) from Python to the DB
-# session = Session(engine)
+session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -43,43 +43,45 @@ app = Flask(__name__)
 #creating welcome page
 @app.route('/')
 def welcome():
-    session = Session(engine)
+    
     #list all routes 
     return (
         f'Available Routes:<br/>'
-        f'/api/v1.0/precipitation'
-        f'/api/v1.0/stations'
-        f'/api/v1.0/tobs'
-        f'/api/v1.0/<start>'
+        f'/api/v1.0/precipitation<br/>'
+        f'/api/v1.0/stations<br/>'
+        f'/api/v1.0/tobs<br/>'
+        f'/api/v1.0/<start><br/>'
         f'/api/v1.0/<start>/<end>'
     )
-# precipitation route
-# @app.route('/api/v1.0/precipitation')
-# def precipitation():
-#     #find the most recent day recorded
-#     recent_rain = session.query(Measurement).filter(Measurement.date).\
-#         order_by(Measurement.date.desc()).first()
-# # Starting from the most recent data point in the database. 
-#     start_date_str = recent_rain.date
-#     start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
-# # Calculate the date one year from the last date in data set.
-#     end_date = start_date - timedelta(days=365)
-# #query date and precipitation with the wanted params
-#     results = session.query(Measurement.date,Measurement.prcp).\
-#                       filter(Measurement.date <= start_date_str,
-#                       Measurement.date >= end_date.strftime('%Y-%m-%d')).all()
-#     precipitation_data = []
-#     for date, prcp in results:
-#         precipitation_dict = {}
-#         precipitation_dict['date']= date
-#         precipitation_dict['precipitatin'] = prcp
-#         precipitation_data.append(precipitation_dict)
-#     return jsonify(precipitation_data)
+#precipitation route
+@app.route('/api/v1.0/precipitation')
+def precipitation():
+    #find the most recent day recorded
+    recent_rain = session.query(Measurement).filter(Measurement.date).\
+        order_by(Measurement.date.desc()).first()
+# Starting from the most recent data point in the database. 
+    start_date_str = recent_rain.date
+    start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
+# Calculate the date one year from the last date in data set.
+    end_date = start_date - timedelta(days=365)
+#query date and precipitation with the wanted params
+    results = session.query(Measurement.date,Measurement.prcp).\
+                      filter(Measurement.date <= start_date_str,
+                      Measurement.date >= end_date.strftime('%Y-%m-%d')).all()
+    precipitation_data = []
+    for date, prcp in results:
+        precipitation_dict = {}
+        precipitation_dict['date']= date
+        precipitation_dict['precipitation'] = prcp
+        precipitation_data.append(precipitation_dict)
+    return jsonify(precipitation_data)
+
 
 #stations route 
-# @app.route('/api/v1.0/stations')
-# def stations():
-#     return
+@app.route('/api/v1.0/stations')
+def stations():
+    
+    return
 
 # #temperature route
 # @app.route('/api/v1.0/tobs')
@@ -96,3 +98,5 @@ def welcome():
 # def start_end_date():
 #     return
 
+if __name__ == "__main__":
+    app.run(debug=True)
